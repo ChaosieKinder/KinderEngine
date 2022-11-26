33abebd208d5807938d0ca164ce2a55da1438a80
Cassandra Kinder <chaosiekinder@gmail.com>
2022-11-26 11:17:15 -0600

    Add static logger. Version bump to 1.0.1-alpha

---
+++ src/KinderEngine.Core/Logging/StaticLoggerFactory.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Concurrent;
+
+namespace KinderEngine.Core.Logging
+{
+    public static class StaticLoggerFactory
+    {
+        private static ILoggerFactory _loggerFactory;
+
+        private static ConcurrentDictionary<Type, ILogger> loggerByType = new();
+
+        public static void Initialize(ILoggerFactory loggerFactory)
+        {
+            if (_loggerFactory is not null)
+                throw new InvalidOperationException("StaticLogger already initialized!");
+
+            _loggerFactory = loggerFactory ?? throw new ArgumentNullException(nameof(loggerFactory));
+        }
+
+        public static ILogger GetStaticLogger<T>()
+        {
+            if (_loggerFactory is null)
+                throw new InvalidOperationException("StaticLogger is not initialized yet.");
+
+            return loggerByType
+                .GetOrAdd(typeof(T), _loggerFactory.CreateLogger<T>());
+        }
+
+        public static WebApplication UseStaticLogger(this WebApplication webApp)
+        {
+            var fac = webApp.Services.GetRequiredService<ILoggerFactory>();
+            StaticLoggerFactory.Initialize(fac);
+            return webApp;
+        }
+    }
+}
